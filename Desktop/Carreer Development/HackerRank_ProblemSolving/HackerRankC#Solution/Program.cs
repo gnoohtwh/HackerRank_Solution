958737405138a5e9a411d9cf354f1361c819d44f
gnoohtwh <thongismyname1507@gmail.com>
2026-01-06 11:41:41 +0700

    successfully solve

---
+++ Desktop/Carreer Development/HackerRank_ProblemSolving/HackerRankC#Solution/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace HackerRankC_Solution;
+
+using System.Collections;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        NeetCode150.ValidAnagram();
+    }
+
+
+}
+class NeetCode150
+{
+    public static void ValidAnagram()
+    {
+        Hashtable countT = new Hashtable();
+        Hashtable countS = new Hashtable();
+
+        string s = "racecar", t = " carrace";
+        if (s.Length != t.Length)
+        {
+            Console.Write(false);
+        }
+
+        foreach (char charac in s)
+        {
+            countS.Add(charac, 1);
+        }
+        Console.WriteLine(countS);
+    }
+
+}
