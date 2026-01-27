6cd43a6dbb7b7d12db72c29d504e0f0f1df05a76
gnoohtwh <thongismyname1507@gmail.com>
2026-01-27 07:44:18 +0700

    easy problem

--- Desktop/Carreer Development/HackerRank_ProblemSolving/HackerRankC#Solution/obj/Debug/net9.0/HackerRankC#Solution.AssemblyInfo.cs
+++ Desktop/Carreer Development/HackerRank_ProblemSolving/HackerRankC#Solution/obj/Debug/net9.0/HackerRankC#Solution.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HackerRankC#Solution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+958737405138a5e9a411d9cf354f1361c819d44f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41a95f998acc401ae25d8e2e016ff252f9d78026")]
 [assembly: System.Reflection.AssemblyProductAttribute("HackerRankC#Solution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HackerRankC#Solution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Desktop/Carreer Development/HackerRank_ProblemSolving/test.py
+++ Desktop/Carreer Development/HackerRank_ProblemSolving/test.py
@@ -1,46 +1,36 @@
-import math as m
-k= 7
-s= [278,576,496,727,410,124,338,149,209,702,282,718,771,575,436]
+# for loop to count how many 'a' character in len(s)->  -> while loop used to divide n by k until n < len(s) (using altNum for n new container) 
+# Checking if string s is empty
+s = "ababa"
+if len(s) == 0:
+    print(0)
 
 
-s = [num%k for num in s] # Creating a list with all the remaining num after divide it to k 
-s = sorted(s)
-print(s)
-res = 0
+# Counting how many 'a' character in the string s
+n = 3
+aCount  = 0 # how many 'a'character are there in string s?
+count = 0 
 
-if s[0] == 0: # If any of those numbers can be divided by k, it will just make the res + 1, but why ? -> because the maximize num cointaing in a subset is only 1 (if there are more than 1 number in that subset, it makes the subset invalid)
-    res += 1
+if n >= len(s):
+    for c in s:
+        if c == 'a':
+            aCount += 1
+    while n >= len(s):
+        count += (n // len(s)) * aCount
+        rem = n % len(s)
 
-sDict = {}
-for num in s:
-    if num != 0:
-        if num in sDict:
-            sDict[num] += 1
+        if rem == 0:
+            break
         else:
-            sDict[num] = 1 
-# Creating the list to count how many num by each categories 
-s = list(sDict.items())
-print(s)
-#binary search the max length of total sub that can not be divided by k
-l, r = 0, len(s)-1
-while l <= r:
-    if s[l][0]+s[r][0] == k:
-        if s[l][0] == k/2:
-            res += 1
-        else:
-            if s[l][1] > s[r][1]: # Maximizing the length of the subset
-                res += s[l][1]
-            else:
-                res += s[r][1]
-        r -= 1
-        l += 1     
-    else:
-        if s[l][0]+s[r][0] > k: # Maximizing the length of the subset
-            res += s[r][1]
-            r -= 1
-        else:
-            res += s[l][1]
-            l += 1
-    
+            if rem < len(s):
+                for i in range(rem):
+                    if s[i]== 'a':
+                        count += 1
+                break
+            elif rem >= len(s):
+                n = rem 
+else:
+    for i in range(n):
+        if s[i] == 'a':
+            count +=1
+print(count)
 
-# print(res)
